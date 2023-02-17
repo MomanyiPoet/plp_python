9b06b7e26fb8263f9ca91ae9edcd2939f9263bc0
Momanyi Poet <101427637+MomanyiPoet@users.noreply.github.com>
2023-02-17 20:48:36 +0300

    Weekly Code Challenge!

---
+++ introduction_python/user_input.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+name = str(input("What is your name: "))
+age = int(input("What is your age: "))
+location = str(input("What is your location: "))
+
+print("Hello {}, you are {} years old and live in {}.".format(name, age, location))
