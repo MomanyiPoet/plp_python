19f80df155121e095f13090c788a2827e259bfc5
Momanyi Poet <101427637+MomanyiPoet@users.noreply.github.com>
2023-02-18 17:31:21 +0300

    tuple containing the names of five of your favorite books.

---
+++ 02_data_structures/2-tuple.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+# Tuple containing favourite books
+favourite_books = (
+    "The 7 Habits of Highly Effective People",
+    "The Power of Positive Thinking",
+    "Drive: The Surprising Truth About What Motivates Us",
+    "Awaken the Giant Within",
+    "Mindset: The New Psychology of Success"
+)
+
+# for loop to print each book
+for i in favourite_books:
+    print(i)
