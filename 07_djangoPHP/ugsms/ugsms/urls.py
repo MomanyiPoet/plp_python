999c29a69caea79b946c960202b6637ba25c862c
Momanyi Poet <momanyiturner@gmail.com>
2023-07-10 16:49:22 +0300

    static files

--- 07_djangoPHP/ugsms/ugsms/urls.py
+++ 07_djangoPHP/ugsms/ugsms/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from ugsms.views import landing_page
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', landing_page),
+    path('main/', include('main.urls')),
 ]
