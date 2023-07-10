2e3c5ec3f718afbcdad457a68ae5641333fe8c57
Momanyi Poet <momanyiturner@gmail.com>
2023-07-10 20:45:40 +0300

    added about page

--- 07_djangoPHP/ugsms/main/urls.py
+++ 07_djangoPHP/ugsms/main/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 #URL Conf
 urlpatterns = [
-    path('index/', views.landing_page)
+    path('index/', views.landing_page),
+    path('about/', views.about),
 ]
--- 07_djangoPHP/ugsms/main/views.py
+++ 07_djangoPHP/ugsms/main/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def landing_page(request):
     return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
