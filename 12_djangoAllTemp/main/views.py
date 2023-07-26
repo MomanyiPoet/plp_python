1544946c1b54e9dc14db6ee81ed4f018377de650
Momanyi Poet <momanyiturner@gmail.com>
2023-07-26 19:07:40 +0300

    added all templates for user and dashboard interface

--- 12_djangoAllTemp/main/views.py
+++ 12_djangoAllTemp/main/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from django.contrib.auth.forms import UserCreationForm
+# from .forms import CreateUserForm
 
 # Create your views here.
+
+def landing_page(request):
+    return render(request, 'user/index.html')
+
+def about(request):
+    return render(request, 'user/about.html')
+
+def services(request):
+    return render(request, 'user/services.html')
+
+def contact(request):
+    return render(request, 'user/contact.html')
+    
+# REGISTRATION OF CLIENTS/USERS
+def signup(request):
+    # if request.method == "POST":
+    #     form = CreateUserForm(request.POST)
+    #     if form.is_valid():
+    #         form.save()
+    #         return redirect('login')
+    # else:
+    #     form = CreateUserForm()
+    # context = {
+    #     'form' : form,
+    # }
+    return render(request, 'user/signup.html',)
+
+def login(request):
+    return render(request, 'user/login.html')
+
+def logout(request):
+    return render(request, 'user/logout.html')
