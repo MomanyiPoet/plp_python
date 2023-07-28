79ab55f2be7faf34ed820230d9938522de3a1fb2
Momanyi Poet <momanyiturner@gmail.com>
2023-07-28 21:47:10 +0300

    changed title

--- 13_djangoAllTempLogin/main/admin.py
+++ 13_djangoAllTempLogin/main/admin.py
@@ -1,11 +1,6 @@
 from django.contrib import admin
 from main.models import aboutDetail, contactDetail
 
-# CHANGING ADMIN TITLES
-admin.site.site_title = 'UTAWALA SGMS'
-admin.site.site_header = 'UTAWALA SERVICE GARAGE MANAGEMENT SYSTEM'
-admin.site.index_title = 'USGMS ADMIN'
-
 # CHANGING TABLE LAYOUT
 class aboutDetailAdmin(admin.ModelAdmin):
     list_display = ('pageName', 'pageDescription')
--- 13_djangoAllTempLogin/userdashboard/admin.py
+++ 13_djangoAllTempLogin/userdashboard/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+# CHANGING ADMIN TITLES
+admin.site.site_title = 'UTAWALA SGMS'
+admin.site.site_header = 'UTAWALA SGMS'
+admin.site.index_title = 'USGMS ADMIN'
+
 # Register your models here.
