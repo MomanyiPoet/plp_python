9fc7bf386419d89ec54f498a8236b272391768be
Momanyi Poet <momanyiturner@gmail.com>
2023-07-31 16:37:25 +0300

    added login

--- 13_djangoAllTempLogin/main/urls.py
+++ 13_djangoAllTempLogin/main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-# from django.contrib.auth import views as auth_views
+from django.contrib.auth import views as auth_views
 
 #URL Conf
 urlpatterns = [
@@ -10,7 +10,6 @@ urlpatterns = [
     path('services/', views.services, name='services'),
     path('contact/', views.contact, name='contact'),
     path('signup/', views.signup, name='signup'),
-    path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout'),
-    # path('logout/', auth_views.LogoutView.as_view(template_name = 'logout.html'), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name = 'user/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name = 'user/logout.html'), name='logout'),
 ]
